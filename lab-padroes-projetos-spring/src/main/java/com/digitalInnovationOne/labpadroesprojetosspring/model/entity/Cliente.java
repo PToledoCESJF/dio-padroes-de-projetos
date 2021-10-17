73313fc4ee0d20adeb908f7420e566c0921dd3ef
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-10-17 19:37:32 -0300

    Creating the entities classes

---
+++ lab-padroes-projetos-spring/src/main/java/com/digitalInnovationOne/labpadroesprojetosspring/model/entity/Cliente.java
@@ -0,0 +1,24 @@
+package com.digitalInnovationOne.labpadroesprojetosspring.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    private Endereco endereco;
+
+}
