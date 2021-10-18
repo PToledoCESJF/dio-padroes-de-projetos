6ec0f43e79080911d46ccf3187d73167c9c1c123
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-10-17 22:30:39 -0300

    Changing repositories to use JpaRepository

--- lab-padroes-projetos-spring/src/main/java/com/digitalInnovationOne/labpadroesprojetosspring/repository/ClienteRepository.java
+++ lab-padroes-projetos-spring/src/main/java/com/digitalInnovationOne/labpadroesprojetosspring/repository/ClienteRepository.java
@@ -1,10 +1,10 @@
 package com.digitalInnovationOne.labpadroesprojetosspring.repository;
 
 import com.digitalInnovationOne.labpadroesprojetosspring.model.entity.Cliente;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 }
--- lab-padroes-projetos-spring/src/main/java/com/digitalInnovationOne/labpadroesprojetosspring/repository/EnderecoRepository.java
+++ lab-padroes-projetos-spring/src/main/java/com/digitalInnovationOne/labpadroesprojetosspring/repository/EnderecoRepository.java
@@ -1,10 +1,10 @@
 package com.digitalInnovationOne.labpadroesprojetosspring.repository;
 
 import com.digitalInnovationOne.labpadroesprojetosspring.model.entity.Endereco;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+public interface EnderecoRepository extends JpaRepository<Endereco, String> {
 
 }
